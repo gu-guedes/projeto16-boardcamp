ed0710329fafc5b47f8a92010e0eea306ac0971a
gu-guedes <guguedes2020@gmail.com>
2023-05-15 17:13:49 -0300

    alteracoes do deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import gamesRouter from "./routes/games.routes.js"
 import customersRouter from "./routes/customers.routes.js"
 import rentalsRouter from "./routes/rentals.routes.js"
+import dotenv from "dotenv"
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(gamesRouter)
 app.use(customersRouter)
 app.use(rentalsRouter)
+dotenv.config()
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
