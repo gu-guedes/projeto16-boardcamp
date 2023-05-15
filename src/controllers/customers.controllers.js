4beb4249e6f9d0df739b283c39776271ef1931ef
gu-guedes <guguedes2020@gmail.com>
2023-05-15 16:04:47 -0300

    versao final para teste 3

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -43,7 +43,7 @@ export async function updateCustomersById(req, res) {
     const { name, phone, cpf, birthday } = req.body
     const { id } = req.params
     try {
-        const foundCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1 AND id != $2;`, [cpf, id])
+        const foundCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1 AND id <> $2;`, [cpf, id])
         if(foundCpf.rowCount !== 0) return res.sendStatus(409)
 
         await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5);`, [name, phone, cpf, birthday, id])
